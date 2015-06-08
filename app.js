653266e6d6dd31cf88fb3ec6bf352ad357a522e2
Hai Do Minh <minhhai3b@gmail.com>
2015-06-08 22:08:05 +0900

    calc base on Math functions

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ function prefixCalc(exp) {
     }
     if ((char === ' ' || i === 0) && token.length > 0) {
       if (calc.isOperator(token)) {
-        var rst = calc(token, Number(stack.pop()), Number(stack.pop()))
+        var rst = calc(token, getValue(stack.pop()), getValue(stack.pop()))
         stack.push(rst)
       } else {
         stack.push(token)
@@ -54,7 +54,7 @@ function postfixCalc (exp) {
     }
     if ((char === ' ' || i === exp.length - 1) && token.length > 0) {
       if (calc.isOperator(token)) {
-        var rst = calc(token, Number(stack.pop()), Number(stack.pop()))
+        var rst = calc(token, getValue(stack.pop()), getValue(stack.pop()))
         stack.push(rst)
       } else {
         stack.push(token)
@@ -64,4 +64,18 @@ function postfixCalc (exp) {
   }
 
   return stack.pop()
+}
+
+function getValue(exp) {
+  if (calc.isNumber(exp)) return Number(exp)
+  var i = exp.indexOf('[')
+  if (i === -1) return calc(exp)
+  var func = exp.substring(0, i),
+      args = exp.substring(i + 1, exp.length - 1)
+  args = args.split(',')
+  args.map(function (val) {
+    return Number(val)
+  })
+  args.unshift(func)
+  return calc.apply(null, args)
 }
