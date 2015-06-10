e5b5ca73d2ba8f88cb1509e9aa6b451fd898d0b6
Do Minh Hai <minhhai3b@gmail.com>
2015-06-10 15:55:10 +0900

    try grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,21 @@
 module.exports = function (grunt) {
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json')
+    // pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+      files: ['Gruntfile.js', '/*.js', 'test/*.js'],
+      options: {
+        globals: {
+          jQuery: true
+        }
+      }
+    }
+  })
+
+  grunt.loadNpmTasks('grunt-contrib-jshint')
+
+  grunt.registerTask('default', ['jshint'])
+
+  grunt.registerTask('custom', 'Log some stuff.', function () {
+    grunt.log.write('Logging some stuff...').ok()
   })
 }
