195a7ac6015f03dee7084d152b2646a81548b9d8
Do Minh Hai <minhhai3b@gmail.com>
2015-06-16 17:11:26 +0900

    change E.x to Ex

--- cli.js
+++ cli.js
@@ -6,10 +6,10 @@ if (process.argv.length < 3) {
   console.error('    $ scalc "your_expression"\n\n    or\n')
   console.error('    $ scalc \'your_expression\'\n\n    or\n')
   console.error('    $ scalc your_expression\n')
-	console.error('    E.x: $ scalc "1 + 5*(PI - 10 * 2^5)" or $ scalc 1 + 5*[PI - 10 * 2^5]')
+	console.error('    Ex: $ scalc "1 + 5*(PI - 10 * 2^5)" or $ scalc 1 + 5*[PI - 10 * 2^5]')
   console.error('\n\n2. API\n')
 	console.error('    var scalc = require(\'scalc\')\n    var result = scalc(your_expression)\n')
-  console.error('    E.x: var result = require(\'scalc\')(\'10 - E * |-10| - 0.5*(2^3)\')')
+  console.error('    Ex: var result = require(\'scalc\')(\'10 - E * |-10| - 0.5*(2^3)\')')
 } else {
   var rst = require('./app')(process.argv.slice(2).join(' ').trim())
   console.log(rst)
