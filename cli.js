ea97a66553716ab6f4d37e3ab6a37abc0cc0141a
Do Minh Hai <minhhai3b@gmail.com>
2015-06-16 16:50:22 +0900

    add usage for cli and remove grunt

--- cli.js
+++ cli.js
@@ -1,4 +1,17 @@
 #!/usr/bin/env node
 
+if (process.argv.length < 3) {
+  console.error("Smart Calculator for Nodejs supports all type of Polish notations.\n")
+  console.error("Usage\n\n1. Command Line\n")
+  console.error("    $ scalc \"your_expression\"\n\n    or\n")
+  console.error("    $ scalc 'your_expression'\n\n    or\n")
+  console.error("    $ scalc your_expression\n")
+	console.error("    E.x: $ scalc \"1 + 5*(PI - 10 * 2^5)\" or $ scalc 1 + 5*(PI - 10 * 2^5)")
+  console.error("\n\n2. API\n")
+	console.error("    var scalc = require('scalc')\n    var result = scalc(your_expression)\n")
+  console.error("    E.x: var result = require('scalc')('10 - E * |-10| - 0.5*(2^3)')")
+  return
+}
+
 var rst = require('./app')(process.argv.slice(2).join(' ').trim())
 console.log(rst)
