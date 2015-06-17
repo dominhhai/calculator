5b37b5fcbae031e247b66617ab30b7e386298fbc
Do Minh Hai <minhhai3b@gmail.com>
2015-06-17 17:47:06 +0900

    show version info

--- cli.js
+++ cli.js
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
 
 if (process.argv.length < 3) {
+  var version = require('./package').version
   console.error('Smart Calculator for Nodejs supports all type of Polish notations.\n')
+  console.error('Version: ', version,'\n')
   console.error('Usage\n\n1. Command Line\n')
   console.error('    $ scalc "your_expression"\n\n    or\n')
   console.error('    $ scalc \'your_expression\'\n\n    or\n')
